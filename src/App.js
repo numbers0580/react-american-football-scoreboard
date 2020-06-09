34f18b8484ed98d5e43598c3d10db2a144f7f860
numbers0580 <koreankid18@yahoo.com>
2020-06-08 20:00:40 -0600

    Completed one of the stretches -- the timer. Honestly, I only did it to try to help out Charlie

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import BottomRow from "./BottomRow";
 import "./App.css";
 
@@ -7,6 +7,11 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, homeChange] = useState(17);
   const [awayScore, awayChange] = useState(21);
+
+  //New constants for the useEffect function below
+  const [mins, updateMinutes] = useState(14);
+  const [sec1, updateSec1] = useState(5);
+  const [sec2, updateSec2] = useState(9);
 
   const homeTD = htdEvent => {
     homeChange(homeScore + 7);
@@ -21,6 +26,21 @@ function App() {
     awayChange(awayScore + 3);
   };
 
+  //Created this only to see if I could help Charlie out
+  useEffect(() => {
+    setTimeout(() => {
+      updateSec2(sec2 - 1);
+      if(sec2 === 0) {
+        updateSec2(9);
+        updateSec1(sec1 - 1);
+        if(sec1 === 0) {
+          updateSec1(5);
+          updateMinutes(mins - 1);
+        }
+      }
+    }, 1000);
+  });
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -31,7 +51,7 @@ function App() {
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeScore}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer">{mins}:{sec1}{sec2}</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{awayScore}</div>
